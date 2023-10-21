713d249a9644fe25ccfecf8e9a59ba66bdceee9f
teguh <teguhbendomalang22@gmail.com>
2023-10-21 12:25:03 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const { 
+    addNotehandler, 
+    getAllNotesHandler, 
+    getNoteByIdHandler, 
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNotehandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    }
+];
+
+module.exports = routes;
